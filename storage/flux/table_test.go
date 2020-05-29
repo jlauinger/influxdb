8ff1511d12c6339d59884d8f12e40b199e43e4b4
Jonathan A. Sternberg <jonathan@influxdata.com>
2020-05-29 12:00:47 -0500

    work on test cases for the window aggregate reader

--- storage/flux/table_test.go
+++ storage/flux/table_test.go
@@ -12,8 +12,10 @@ import (
 
 	"github.com/influxdata/flux"
 	"github.com/influxdata/flux/execute"
+	"github.com/influxdata/flux/execute/executetest"
 	"github.com/influxdata/flux/memory"
 	"github.com/influxdata/flux/values"
+	"github.com/influxdata/influxdb/v2"
 	"github.com/influxdata/influxdb/v2/cmd/influxd/generate"
 	"github.com/influxdata/influxdb/v2/mock"
 	"github.com/influxdata/influxdb/v2/models"
@@ -25,104 +27,212 @@ import (
 	"go.uber.org/zap/zaptest"
 )
 
-func BenchmarkReadFilter(b *testing.B) {
+type SetupFunc func(org, bucket influxdb.ID) (gen.SeriesGenerator, gen.TimeRange)
+
+type StorageReader struct {
+	Org    influxdb.ID
+	Bucket influxdb.ID
+	Bounds execute.Bounds
+	Close  func()
+	query.StorageReader
+}
+
+func NewStorageReader(tb testing.TB, setupFn SetupFunc) *StorageReader {
+	logger := zaptest.NewLogger(tb)
+	rootDir, err := ioutil.TempDir("", "storage-flux-test")
+	if err != nil {
+		tb.Fatal(err)
+	}
+	close := func() { _ = os.RemoveAll(rootDir) }
+
 	idgen := mock.NewMockIDGenerator()
-	tagsSpec := &gen.TagsSpec{
-		Tags: []*gen.TagValuesSpec{
-			{
-				TagKey: "t0",
-				Values: func() gen.CountableSequence {
-					return gen.NewCounterByteSequence("a-%d", 0, 5)
-				},
-			},
-			{
-				TagKey: "t1",
-				Values: func() gen.CountableSequence {
-					return gen.NewCounterByteSequence("b-%d", 0, 1000)
-				},
-			},
+	org, bucket := idgen.ID(), idgen.ID()
+	sg, tr := setupFn(org, bucket)
+
+	generator := generate.Generator{}
+	if _, err := generator.Run(context.Background(), rootDir, sg); err != nil {
+		tb.Fatal(err)
+	}
+
+	enginePath := filepath.Join(rootDir, "engine")
+	engine := storage.NewEngine(enginePath, storage.NewConfig())
+	engine.WithLogger(logger)
+
+	if err := engine.Open(context.Background()); err != nil {
+		tb.Fatal(err)
+	}
+	reader := storageflux.NewReader(readservice.NewStore(engine))
+	return &StorageReader{
+		Org:    org,
+		Bucket: bucket,
+		Bounds: execute.Bounds{
+			Start: values.ConvertTime(tr.Start),
+			Stop:  values.ConvertTime(tr.End),
 		},
+		Close:         close,
+		StorageReader: reader,
 	}
-	spec := gen.Spec{
-		OrgID:    idgen.ID(),
-		BucketID: idgen.ID(),
-		Measurements: []gen.MeasurementSpec{
-			{
-				Name:     "m0",
-				TagsSpec: tagsSpec,
-				FieldValuesSpec: &gen.FieldValuesSpec{
-					Name: "f0",
-					TimeSequenceSpec: gen.TimeSequenceSpec{
-						Count: math.MaxInt32,
-						Delta: time.Minute,
+}
+
+func (r *StorageReader) ReadWindowAggregate(ctx context.Context, spec query.ReadWindowAggregateSpec, alloc *memory.Allocator) (query.TableIterator, error) {
+	wr := r.StorageReader.(query.WindowAggregateReader)
+	return wr.ReadWindowAggregate(ctx, spec, alloc)
+}
+
+func TestStorageReader_ReadWindowAggregate(t *testing.T) {
+	reader := NewStorageReader(t, func(org, bucket influxdb.ID) (gen.SeriesGenerator, gen.TimeRange) {
+		// generate a consistent output using the generator api
+		// use this generator instead of the random one used by the benchmark.
+		// gen.NewFloatArrayValuesSequence()
+		panic("implement me")
+	})
+
+	mem := &memory.Allocator{}
+	ti, err := reader.ReadWindowAggregate(context.Background(), query.ReadWindowAggregateSpec{}, mem)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := executetest.NewResult([]*executetest.Table{
+		// fill in expected tables
+	})
+	want.Normalize()
+
+	gotTables := []*executetest.Table{}
+	if err := ti.Do(func(table flux.Table) error {
+		t, err := executetest.ConvertTable(table)
+		if err != nil {
+			return err
+		}
+		gotTables = append(gotTables, t)
+		return nil
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	got := executetest.NewResult(gotTables)
+	got.Normalize()
+
+	// compare these two
+
+	// logger := zaptest.NewLogger(b)
+	// rootDir, err := ioutil.TempDir("", "storage-reads-test")
+	// if err != nil {
+	// 	b.Fatal(err)
+	// }
+	// defer func() { _ = os.RemoveAll(rootDir) }()
+	//
+	// generator := generate.Generator{}
+	// if _, err := generator.Run(context.Background(), rootDir, sg); err != nil {
+	// 	b.Fatal(err)
+	// }
+	//
+	// enginePath := filepath.Join(rootDir, "engine")
+	// engine := storage.NewEngine(enginePath, storage.NewConfig())
+	// engine.WithLogger(logger)
+	//
+	// if err := engine.Open(context.Background()); err != nil {
+	// 	b.Fatal(err)
+	// }
+	// reader := storageflux.NewReader(readservice.NewStore(engine))
+}
+
+func BenchmarkReadFilter(b *testing.B) {
+	setupFn := func(org, bucket influxdb.ID) (gen.SeriesGenerator, gen.TimeRange) {
+		tagsSpec := &gen.TagsSpec{
+			Tags: []*gen.TagValuesSpec{
+				{
+					TagKey: "t0",
+					Values: func() gen.CountableSequence {
+						return gen.NewCounterByteSequence("a-%d", 0, 5)
 					},
-					DataType: models.Float,
-					Values: func(spec gen.TimeSequenceSpec) gen.TimeValuesSequence {
-						r := rand.New(rand.NewSource(10))
-						return gen.NewTimeFloatValuesSequence(
-							spec.Count,
-							gen.NewTimestampSequenceFromSpec(spec),
-							gen.NewFloatRandomValuesSequence(0, 90, r),
-						)
+				},
+				{
+					TagKey: "t1",
+					Values: func() gen.CountableSequence {
+						return gen.NewCounterByteSequence("b-%d", 0, 1000)
 					},
 				},
 			},
-			{
-				Name:     "m0",
-				TagsSpec: tagsSpec,
-				FieldValuesSpec: &gen.FieldValuesSpec{
-					Name: "f1",
-					TimeSequenceSpec: gen.TimeSequenceSpec{
-						Count: math.MaxInt32,
-						Delta: time.Minute,
+		}
+		spec := gen.Spec{
+			OrgID:    org,
+			BucketID: bucket,
+			Measurements: []gen.MeasurementSpec{
+				{
+					Name:     "m0",
+					TagsSpec: tagsSpec,
+					FieldValuesSpec: &gen.FieldValuesSpec{
+						Name: "f0",
+						TimeSequenceSpec: gen.TimeSequenceSpec{
+							Count: math.MaxInt32,
+							Delta: time.Minute,
+						},
+						DataType: models.Float,
+						Values: func(spec gen.TimeSequenceSpec) gen.TimeValuesSequence {
+							r := rand.New(rand.NewSource(10))
+							return gen.NewTimeFloatValuesSequence(
+								spec.Count,
+								gen.NewTimestampSequenceFromSpec(spec),
+								gen.NewFloatRandomValuesSequence(0, 90, r),
+							)
+						},
 					},
-					DataType: models.Float,
-					Values: func(spec gen.TimeSequenceSpec) gen.TimeValuesSequence {
-						r := rand.New(rand.NewSource(11))
-						return gen.NewTimeFloatValuesSequence(
-							spec.Count,
-							gen.NewTimestampSequenceFromSpec(spec),
-							gen.NewFloatRandomValuesSequence(0, 180, r),
-						)
+				},
+				{
+					Name:     "m0",
+					TagsSpec: tagsSpec,
+					FieldValuesSpec: &gen.FieldValuesSpec{
+						Name: "f1",
+						TimeSequenceSpec: gen.TimeSequenceSpec{
+							Count: math.MaxInt32,
+							Delta: time.Minute,
+						},
+						DataType: models.Float,
+						Values: func(spec gen.TimeSequenceSpec) gen.TimeValuesSequence {
+							r := rand.New(rand.NewSource(11))
+							return gen.NewTimeFloatValuesSequence(
+								spec.Count,
+								gen.NewTimestampSequenceFromSpec(spec),
+								gen.NewFloatRandomValuesSequence(0, 180, r),
+							)
+						},
+					},
+				},
+				{
+					Name:     "m0",
+					TagsSpec: tagsSpec,
+					FieldValuesSpec: &gen.FieldValuesSpec{
+						Name: "f1",
+						TimeSequenceSpec: gen.TimeSequenceSpec{
+							Count: math.MaxInt32,
+							Delta: time.Minute,
+						},
+						DataType: models.Float,
+						Values: func(spec gen.TimeSequenceSpec) gen.TimeValuesSequence {
+							r := rand.New(rand.NewSource(12))
+							return gen.NewTimeFloatValuesSequence(
+								spec.Count,
+								gen.NewTimestampSequenceFromSpec(spec),
+								gen.NewFloatRandomValuesSequence(10, 10000, r),
+							)
+						},
 					},
 				},
 			},
-			{
-				Name:     "m0",
-				TagsSpec: tagsSpec,
-				FieldValuesSpec: &gen.FieldValuesSpec{
-					Name: "f1",
-					TimeSequenceSpec: gen.TimeSequenceSpec{
-						Count: math.MaxInt32,
-						Delta: time.Minute,
-					},
-					DataType: models.Float,
-					Values: func(spec gen.TimeSequenceSpec) gen.TimeValuesSequence {
-						r := rand.New(rand.NewSource(12))
-						return gen.NewTimeFloatValuesSequence(
-							spec.Count,
-							gen.NewTimestampSequenceFromSpec(spec),
-							gen.NewFloatRandomValuesSequence(10, 10000, r),
-						)
-					},
-				},
-			},
-		},
+		}
+		tr := gen.TimeRange{
+			Start: mustParseTime("2019-11-25T00:00:00Z"),
+			End:   mustParseTime("2019-11-26T00:00:00Z"),
+		}
+		return gen.NewSeriesGeneratorFromSpec(&spec, tr), tr
 	}
-	tr := gen.TimeRange{
-		Start: mustParseTime("2019-11-25T00:00:00Z"),
-		End:   mustParseTime("2019-11-26T00:00:00Z"),
-	}
-	sg := gen.NewSeriesGeneratorFromSpec(&spec, tr)
-	benchmarkRead(b, sg, func(r query.StorageReader) error {
+	benchmarkRead(b, setupFn, func(r *StorageReader) error {
 		mem := &memory.Allocator{}
 		tables, err := r.ReadFilter(context.Background(), query.ReadFilterSpec{
-			OrganizationID: spec.OrgID,
-			BucketID:       spec.BucketID,
-			Bounds: execute.Bounds{
-				Start: values.ConvertTime(tr.Start),
-				Stop:  values.ConvertTime(tr.End),
-			},
+			OrganizationID: r.Org,
+			BucketID:       r.Bucket,
+			Bounds:         r.Bounds,
 		}, mem)
 		if err != nil {
 			return err
@@ -134,27 +244,9 @@ func BenchmarkReadFilter(b *testing.B) {
 	})
 }
 
-func benchmarkRead(b *testing.B, sg gen.SeriesGenerator, f func(r query.StorageReader) error) {
-	logger := zaptest.NewLogger(b)
-	rootDir, err := ioutil.TempDir("", "storage-reads-test")
-	if err != nil {
-		b.Fatal(err)
-	}
-	defer func() { _ = os.RemoveAll(rootDir) }()
-
-	generator := generate.Generator{}
-	if _, err := generator.Run(context.Background(), rootDir, sg); err != nil {
-		b.Fatal(err)
-	}
-
-	enginePath := filepath.Join(rootDir, "engine")
-	engine := storage.NewEngine(enginePath, storage.NewConfig())
-	engine.WithLogger(logger)
-
-	if err := engine.Open(context.Background()); err != nil {
-		b.Fatal(err)
-	}
-	reader := storageflux.NewReader(readservice.NewStore(engine))
+func benchmarkRead(b *testing.B, setupFn SetupFunc, f func(r *StorageReader) error) {
+	reader := NewStorageReader(b, setupFn)
+	defer reader.Close()
 
 	b.ResetTimer()
 	b.ReportAllocs()
